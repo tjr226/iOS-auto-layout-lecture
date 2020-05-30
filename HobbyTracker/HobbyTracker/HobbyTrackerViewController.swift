2eccc96d2dba2dc4fabde847c8fda7a758552a7f
tjr226 <tjr226@gmail.com>
2020-05-30 12:18:17 -0400

    guided project follow along complete

---
+++ HobbyTracker/HobbyTracker/HobbyTrackerViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  HobbyTracker
+//
+//  Created by Timothy Rooney on 5/30/20.
+//  Copyright © 2020 Timothy Rooney. All rights reserved.
+//
+
+import UIKit
+
+class HobbyTrackerViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
